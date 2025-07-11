7d0eabbffaf4b3765d428ccd0fd234a859ff5ff2
Ashley <lingxichen1998@gmail.com>
2025-07-11 17:32:50 -0400

    Lock the box

---
+++ Assets/Scripts/Interact/Level1/LockOnPosition.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+
+public class LockOnCollision : MonoBehaviour
+{
+    private Rigidbody rb;
+    private bool isLocked = false;
+    private Vector3 lockedPosition;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (!isLocked && collision.collider.CompareTag("StopZone"))
+        {
+            LockObject();
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (!isLocked && other.CompareTag("StopZone"))
+        {
+            LockObject();
+        }
+    }
+
+    void LockObject()
+    {
+        isLocked = true;
+
+        // 记住当前锁定位置
+        lockedPosition = transform.position;
+
+        // 停止物理行为
+        rb.linearVelocity = Vector3.zero;
+        rb.angularVelocity = Vector3.zero;
+        rb.useGravity = false;
+        rb.isKinematic = true;
+
+        // 可选：禁用其他控制脚本，比如拖拽等
+        // GetComponent<YourDragScript>()?.enabled = false;
+    }
+
+    void LateUpdate()
+    {
+        if (isLocked && transform.position != lockedPosition)
+        {
+            transform.position = lockedPosition;
+        }
+    }
+}
