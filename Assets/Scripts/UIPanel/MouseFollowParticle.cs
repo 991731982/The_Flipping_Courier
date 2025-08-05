4584b0197ca49f5d2667cb875d13fea3cc5eae13
Ashley <lingxichen1998@gmail.com>
2025-08-05 17:53:59 -0400

    add pause menu UI and mouse particle effect

---
+++ Assets/Scripts/UIPanel/MouseFollowParticle.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class MouseFollowParticle : MonoBehaviour
+{
+    public Camera mainCamera;
+    private static MouseFollowParticle instance;
+
+    void Awake()
+    {
+      
+        if (instance != null && instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    void Start()
+    {
+        if (mainCamera == null)
+            mainCamera = Camera.main;
+    }
+
+    void Update()
+    {
+       
+        if (mainCamera == null)
+            mainCamera = Camera.main;
+
+        Vector3 mousePosition = Input.mousePosition;
+        mousePosition.z = 10f;
+        Vector3 worldPosition = mainCamera.ScreenToWorldPoint(mousePosition);
+        transform.position = worldPosition;
+    }
+}
